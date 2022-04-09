476c489f534d136076e79759c605cfb0767b0ff9
nmustakin <nmust004@ucr.edu>
2022-04-09 11:21:45 -0700

    initialize template

---
+++ kernel.cu
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define TILE_SIZE 16
+
+__global__ void matAdd(int dim, const float *A, const float *B, float* C) {
+
+    /********************************************************************
+     *
+     * Compute C = A + B
+     *   where A is a (dim x dim) matrix
+     *   where B is a (dim x dim) matrix
+     *   where C is a (dim x dim) matrix
+     *
+     ********************************************************************/
+
+    /*************************************************************************/
+    // INSERT KERNEL CODE HERE
+        
+    /*************************************************************************/
+
+}
+
+void basicMatAdd(int dim, const float *A, const float *B, float *C)
+{
+    // Initialize thread block and kernel grid dimensions ---------------------
+
+    const unsigned int BLOCK_SIZE = TILE_SIZE;
+	
+    /*************************************************************************/
+    //INSERT CODE HERE
+
+    /*************************************************************************/
+	
+	// Invoke CUDA kernel -----------------------------------------------------
+
+    /*************************************************************************/
+    //INSERT CODE HERE
+	
+    /*************************************************************************/
+
+}
+
